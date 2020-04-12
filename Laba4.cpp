ac27076782e9c8f7edb4752c5d082d91ec36adbe
Savkin Nikita <Ssaukinmikita@gmail.com>
2020-04-12 14:13:07 +0300

    Laba4 t-1 v4

---
+++ Laba4.cpp
@@ -0,0 +1,192 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//Selection Sort
+void swap(int* xp, int* yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_idx;
+
+    // One by one move boundary of unsorted subarray  
+    for (i = 0; i < n - 1; i++)
+    {
+        // Find the minimum element in unsorted array  
+        min_idx = i;
+        for (j = i + 1; j < n; j++)
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+
+        // Swap the found minimum element with the first element  
+        swap(&arr[min_idx], &arr[i]);
+    }
+}
+// The end of selection sort
+
+/* Function to sort an array using insertion sort*/
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        /* Move elements of arr[0..i-1], that are
+        greater than key, to one position ahead
+        of their current position */
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+// The end insertion sort
+
+// A function to implement bubble sort  
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+
+        // Last i elements are already in place  
+        for (j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(&arr[j], &arr[j + 1]);
+}
+
+// The end bubble sort
+
+/* This function takes last element as pivot, places
+the pivot element at its correct position in sorted
+array, and places all smaller (smaller than pivot)
+to left of pivot and all greater elements to right
+of pivot */
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high]; // pivot  
+    int i = (low - 1); // Index of smaller element  
+
+    for (int j = low; j <= high - 1; j++)
+    {
+        // If current element is smaller than the pivot  
+        if (arr[j] < pivot)
+        {
+            i++; // increment index of smaller element  
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+/* The main function that implements QuickSort
+arr[] --> Array to be sorted,
+low --> Starting index,
+high --> Ending index */
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now
+        at right place */
+        int pi = partition(arr, low, high);
+
+        // Separately sort elements before  
+        // partition and after partition  
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+// The end Quick sort
+
+// To heapify a subtree rooted with node i which is 
+// an index in arr[]. n is size of heap 
+void heapify(int arr[], int n, int i)
+{
+    int largest = i; // Initialize largest as root 
+    int l = 2 * i + 1; // left = 2*i + 1 
+    int r = 2 * i + 2; // right = 2*i + 2 
+
+    // If left child is larger than root 
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+
+    // If right child is larger than largest so far 
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+
+    // If largest is not root 
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+
+        // Recursively heapify the affected sub-tree 
+        heapify(arr, n, largest);
+    }
+}
+
+// main function to do heap sort 
+void heapSort(int arr[], int n)
+{
+    // Build heap (rearrange array) 
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    // One by one extract an element from heap 
+    for (int i = n - 1; i >= 0; i--)
+    {
+        // Move current root to end 
+        swap(arr[0], arr[i]);
+
+        // call max heapify on the reduced heap 
+        heapify(arr, i, 0);
+    }
+}
+
+//The end Heap sort
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+/* Driver code */
+ 
+int main()
+{
+    int arr[] = { 64, 25, 12, 22, 11, 23, 43, 33, 58 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    selectionSort(arr, n);
+    cout << "Sorted array(selectionSort): \n";
+    printArray(arr, n);
+    insertionSort(arr, n);
+    cout << "Sorted array(insertionSort): \n";
+    printArray(arr, n);
+    bubbleSort(arr, n);
+    cout << "Sorted array(bubbleSort): \n";
+    printArray(arr, n);
+    quickSort(arr, n, n);
+    cout << "Sorted array( quickSort): \n";
+    printArray(arr, n);
+    heapSort(arr, n);
+    cout << "Sorted array(heapSort): \n";
+    printArray(arr, n);
+    system("pause");
+    return 0;
+}
